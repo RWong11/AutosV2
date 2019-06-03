875e5efc0fd0743a5bb0498beff56d0bd8d62cf5
MauricioGR15 <09mgr.15@gmail.com>
2019-06-02 20:57:01 -0600

    Error en el codigo, se actualizo el fragmento de personas y servicios las clases Java respectivas

--- app/src/main/java/com/example/autosv2/PersonasFragment.java
+++ app/src/main/java/com/example/autosv2/PersonasFragment.java
@@ -1,16 +1,22 @@
 package com.example.autosv2;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class PersonasFragment extends Fragment {
+public class PersonasFragment extends Fragment implements View.OnClickListener{
     BaseDeDatos bd;
+    EditText tRfc, tNombre, tCiudad;
 
     @Nullable
     @Override
@@ -19,9 +25,129 @@ public class PersonasFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_personas, container, false);
     }
 
-    @Override
+
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         bd = BaseDeDatos.getBD();
+
+        tRfc = view.findViewById(R.id.txtRFC);
+        tNombre = view.findViewById(R.id.txtNombre);
+        tCiudad = view.findViewById(R.id.txtCiudad);
+        (view.findViewById(R.id.botonInsertar)).setOnClickListener(this);
+        (view.findViewById(R.id.botonActualizar)).setOnClickListener(this);
+        (view.findViewById(R.id.botonConsultar)).setOnClickListener(this);
+        (view.findViewById(R.id.botonEliminar)).setOnClickListener(this);
+    }
+
+    public void onClick(View v){
+
+        SQLiteDatabase query = null;
+        String sql,rfc,nombre,ciudad;
+        rfc = tRfc.getText().toString();
+
+        switch(v.getId()){
+            case R.id.botonInsertar:
+
+                nombre = tNombre.getText().toString();
+                ciudad = tCiudad.getText().toString();
+
+                if(rfc.isEmpty() || nombre.isEmpty() || ciudad.isEmpty()){
+                    alerta("Error", "Algunos campos estan vacíos");
+                    return;
+                }
+
+                if(!bd.insertarPersona(rfc,nombre,ciudad))
+                    alerta("BD Error", "Ya existe una persona con el RFC: " + rfc);
+                else
+                    vaciarCampos();
+
+                break;
+
+            case R.id.botonConsultar:
+                if(rfc.isEmpty()){
+                    alerta("Error", "No ha especificado ningún RFC");
+                    return;
+                }
+
+                query = bd.getReadableDatabase();
+                sql = "SELECT * FROM personas WHERE per_rfc=?";
+                Cursor c = query.rawQuery(sql, new String[]{rfc});
+                if(c.moveToFirst()) {
+                    tRfc.setText(c.getString(1));
+                    tNombre.setText(c.getString(2));
+                    tCiudad.setText(c.getString(3));
+
+                    if(c.getInt(4) == 1)
+                        alerta("Alerta", "El RFC que se muestra está dado de baja");
+                }
+                else
+                    Toast.makeText(getActivity(), "RFC no encontrado", Toast.LENGTH_LONG).show();
+
+                query.close();
+                break;
+
+            case R.id.botonActualizar:
+                rfc = tRfc.getText().toString();
+                nombre = tNombre.getText().toString();
+                ciudad = tCiudad.getText().toString();
+
+                if(rfc.isEmpty() || nombre.isEmpty() || ciudad.isEmpty()){
+                    alerta("Error", "Algunos campos están vacíos");
+                    return;
+                }
+
+                query = bd.getWritableDatabase();
+                ContentValues cv = new ContentValues();
+                cv.put("per_rfc", rfc);
+                cv.put("per_nombre", nombre);
+                cv.put("per_ciudad", ciudad);
+
+                if(query.update("personas", cv, "per_rfc=?", new String[]{rfc}) > 0) {
+                    Toast.makeText(getActivity(), "Se ha actualizado el RFC " + rfc, Toast.LENGTH_LONG).show();
+                    vaciarCampos();
+                }
+                else
+                    alerta("Error", "El número de RFC ingresado no existe");
+
+                query.close();
+                break;
+
+            case R.id.botonEliminar:
+                if(rfc.isEmpty()) {
+                    alerta("Error", "No ha especificado ningún RFC");
+                    return;
+                }
+
+                query = bd.getWritableDatabase();
+                ContentValues contenedor = new ContentValues();
+                contenedor.put("per_estado", 1);
+
+                if(query.update("autos", contenedor, "aut_placa=?", new String[]{rfc}) > 0) {
+                    Toast.makeText(getActivity(), "Se ha dado de baja el RFC: " + rfc, Toast.LENGTH_LONG).show();
+                    vaciarCampos();
+                }
+                else
+                    alerta("Error", "El número de RFC ingresado no existe");
+
+                query.close();
+                break;
+        }
+
+
+    }
+
+
+    public void vaciarCampos(){
+        tRfc.setText("");
+        tNombre.setText("");
+        tCiudad.setText("");
+        tRfc.requestFocus();
+    }
+
+    public void alerta(String titulo, String mensaje) {
+        AlertDialog alertDialog = new AlertDialog.Builder(getActivity()).create();
+        alertDialog.setTitle(titulo);
+        alertDialog.setMessage(mensaje);
+        alertDialog.show();
     }
 }
--- app/src/main/java/com/example/autosv2/ServiciosFragment.java
+++ app/src/main/java/com/example/autosv2/ServiciosFragment.java
@@ -1,27 +1,163 @@
 package com.example.autosv2;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class ServiciosFragment extends Fragment {
+public class ServiciosFragment extends Fragment implements View.OnClickListener {
     BaseDeDatos bd;
+    EditText tPlaca, tRfc, tKm, tPrecio, tFecha;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         getActivity().setTitle("Nisson - Servicios");
         return inflater.inflate(R.layout.fragment_servicios, container, false);
+
+
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         bd = BaseDeDatos.getBD();
+
+
+        tPlaca = view.findViewById(R.id.txtPlaca2);
+        tRfc = view.findViewById(R.id.txtRfc2);
+        tKm = view.findViewById(R.id.txtKm);
+        tPrecio = view.findViewById(R.id.txtPrecio);
+        tFecha = view.findViewById(R.id.txtFecha);
+
+        (view.findViewById(R.id.botonInsertar)).setOnClickListener(this);
+        (view.findViewById(R.id.botonActualizar)).setOnClickListener(this);
+        (view.findViewById(R.id.botonConsultar)).setOnClickListener(this);
+        (view.findViewById(R.id.botonEliminar)).setOnClickListener(this);
     }
+
+
+    public void alerta(String titulo, String mensaje) {
+        AlertDialog alertDialog = new AlertDialog.Builder(getActivity()).create();
+        alertDialog.setTitle(titulo);
+        alertDialog.setMessage(mensaje);
+        alertDialog.show();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        SQLiteDatabase query = null;
+        String sql, placa,rfc,km,precio,fecha;
+        placa = tPlaca.getText().toString();
+        rfc = tRfc.getText().toString();
+
+        switch(v.getId()) {
+            case R.id.botonInsertar:
+                km = tKm.getText().toString();
+                precio = tPrecio.getText().toString();
+                fecha = tFecha.getText().toString();
+
+                if(placa.isEmpty() || rfc.isEmpty() || km.isEmpty() || precio.isEmpty() || fecha.isEmpty()) {
+                    alerta("Error", "Algunos campos estan vacios");
+                    return;
+                }
+
+                if(!bd.i)
+                    alerta("BD Error", "Ya existe un auto con la placa: " + placa);
+                else
+                    vaciarCampos();
+
+                break;
+
+            case R.id.botonConsultar:
+                if(placa.isEmpty()) {
+                    alerta("Error", "No ha especificado ninguna placa.");
+                    return;
+                }
+
+                query = bd.getReadableDatabase();
+                sql = "SELECT * FROM autos WHERE aut_placa=?";
+                Cursor c = query.rawQuery(sql, new String[]{placa});
+                if(c.moveToFirst()) {
+                    textoMarca.setText(c.getString(1));
+                    textoModelo.setText(c.getString(2));
+                    byteArrayToImagen(imagenAuto, c.getBlob(3));
+                    textoAno.setText(c.getString(4));
+
+                    if(c.getInt(5) == 1)
+                        alerta("Alerta", "El auto que se muestra está dado de baja");
+                }
+                else
+                    Toast.makeText(getActivity(), "Auto no encontrado", Toast.LENGTH_LONG).show();
+
+                query.close();
+                break;
+            case R.id.botonActualizar:
+                marca = textoMarca.getText().toString();
+                modelo = textoModelo.getText().toString();
+                ano = textoAno.getText().toString();
+
+                if(placa.isEmpty() || marca.isEmpty() || modelo.isEmpty() || ano.isEmpty()) {
+                    alerta("Error", "Algunos campos están vacíos");
+                    return;
+                }
+
+                query = bd.getWritableDatabase();
+                ContentValues cv = new ContentValues();
+                cv.put("aut_marca", marca);
+                cv.put("aut_modelo", modelo);
+                cv.put("aut_imagen", imagenToByteArray(imagenAuto));
+                cv.put("aut_ano", ano);
+
+                if(query.update("autos", cv, "aut_placa=?", new String[]{placa}) > 0) {
+                    Toast.makeText(getActivity(), "Se ha actualizado el auto " + placa, Toast.LENGTH_LONG).show();
+                    vaciarCampos();
+                }
+                else
+                    alerta("Error", "El número de placa ingresado no existe");
+
+                query.close();
+                break;
+            case R.id.botonEliminar:
+                if(placa.isEmpty()) {
+                    alerta("Error", "No ha especificado ninguna placa.");
+                    return;
+                }
+
+                query = bd.getWritableDatabase();
+                ContentValues contenedor = new ContentValues();
+                contenedor.put("aut_estado", 1);
+
+                if(query.update("autos", contenedor, "aut_placa=?", new String[]{placa}) > 0) {
+                    Toast.makeText(getActivity(), "Se ha dado de baja el auto: " + placa, Toast.LENGTH_LONG).show();
+                    vaciarCampos();
+                }
+                else
+                    alerta("Error", "El número de placa ingresado no existe");
+
+                query.close();
+                break;
+        }
+
+    }
+
+    public void vaciarCampos() {
+        tPlaca.setText("");
+        tRfc.setText("");
+        tKm.setText("");
+        tPrecio.setText("");
+        tFecha.setText("");
+        tPlaca.requestFocus();
+    }
+
 }
